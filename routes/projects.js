ecbced2c1f37146f90ae81f938910bf7bcf9bc76
Mac Mittereder <macmitt2300@gmail.com>
2016-12-29 23:58:35 -0500

    Added more pages

--- routes/projects.js
+++ routes/projects.js
@@ -2,8 +2,20 @@ var express = require('express');
 
 var router = express.Router();
 
-router.get('/', (req, res, next) => {
+router.get('/', function (req, res, next) {
    res.render('projects');
+});
+
+router.get('/personalprojects', function (req, res, next) {
+  res.render('personalprojects');
+});
+
+router.get('/contractorprojects', function (req, res, next) {
+  res.render('contractorprojects');
+});
+
+router.get('/inprogressprojects', function (req, res, next) {
+  res.render('inprogressprojects');
 });
 
 module.exports = router;
