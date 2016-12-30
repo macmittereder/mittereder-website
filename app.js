656cefb9d58fcaf0d58c4181aee4e045a2a85764
Mac Mittereder <macmitt2300@gmail.com>
2016-12-29 20:49:19 -0500

    Layout Created

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 var express = require('express');
+var path = require('path');
 
 var app = express();
+
+var nAbout = require('./routes/about');
+var nContact = require('./routes/contact');
+var nProjects = require('./routes/projects');
+var nResume = require('./routes/resume');
 
 app.set('view engine', 'pug');
 
 app.use('/static', express.static('public'));
+app.use('/about', nAbout);
+app.use('/contact', nContact);
+app.use('/projects', nProjects);
+app.use('/resume', nResume);
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
         res.render('index', {title: "Welcome"});
