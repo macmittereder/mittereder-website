9b7429a24bf4d2afeb4187eb3bc8c4784b9fee90
Mac Mittereder <macmitt2300@gmail.com>
2017-01-14 12:00:06 -0500

    Added Subproject Pages

--- routes/projects.js
+++ routes/projects.js
@@ -18,4 +18,28 @@ router.get('/inprogressprojects', function (req, res, next) {
   res.render('inprogressprojects');
 });
 
+router.get('/fastmoviephotos', function (req, res, next) {
+  res.render('fastmoviephotos');
+});
+
+router.get('/lifecounter', function (req, res, next) {
+  res.render('lifecounter');
+});
+
+router.get('/franklinregional', function (req, res, next) {
+  res.render('franklinregional');
+});
+
+router.get('/pinballprice', function (req, res, next) {
+  res.render('pinballprice');
+});
+
+router.get('/wordsandmusic', function(req, res, next) {
+  res.render('wordsandmusic');
+});
+
+router.get('/vehicleapp', function(req, res, next) {
+  res.render('vehicleapp');
+});
+
 module.exports = router;
