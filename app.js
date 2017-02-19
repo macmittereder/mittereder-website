7f638ed88a760afb7db437df7811376a14000e83
Mac Mittereder <macmitt2300@gmail.com>
2017-02-19 11:00:07 -0500

    Changed color scheme and took out about pages

--- app.js
+++ app.js
@@ -3,16 +3,12 @@ var path = require('path');
 
 var app = express();
 
-var nAbout = require('./routes/about');
-var nContact = require('./routes/contact');
 var nProjects = require('./routes/projects');
 var nResume = require('./routes/resume');
 
 app.set('view engine', 'pug');
 
 app.use('/static', express.static('public'));
-app.use('/about', nAbout);
-app.use('/contact', nContact);
 app.use('/projects', nProjects);
 app.use('/resume', nResume);
 
