f3c56f2136326c21aefeb7aa41e582eb408755e0
macmitt2300@gmail.com <macmitt2300@gmail.com>
2022-12-08 21:32:45 -0500

    Updated resume

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,12 +1,18 @@
 const Card = (props) => {
   return (
-    <div
-      className="h-64 w-64 bg-cover m-2"
-      style={{ backgroundImage: `url(${props.image})` }}
-      onClick={props.onClick}
-    >
-      <div className="mt-56 bg-gray-400 ">
-        <p className="items-end p-2">{props.title}</p>
+    <div className="flex justify-center items-center">
+      <div
+        className="w-64 h-64 m-2 justify-center items-center"
+        style={{
+          backgroundImage: `url(${props.image})`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+        }}
+        onClick={props.onClick}
+      >
+        <div className="mt-56 bg-gray-400 ">
+          <p className="items-end p-2">{props.title}</p>
+        </div>
       </div>
     </div>
   );
--- src/Components/Tile/Tile.js
+++ src/Components/Tile/Tile.js
@@ -1,19 +1,10 @@
 const Tile = (props) => {
   return (
     <div className="p-4 rounded-sm shadow-lg py-0 bg-white m-2 flex-1">
-      <p className="text-2xl text-center font-bold">{props.title}</p>
+      <p className="text-4xl text-center font-bold mb-8">{props.title}</p>
       {props.children}
     </div>
   );
 };
 
-const Section = (props) => {
-  return (
-    <div>
-      <p className="text-xl font-bold text-left">{props.title}</p>
-      <p className="text-lg text-left">{props.content}</p>
-    </div>
-  );
-};
-
-export { Tile, Section };
+export default Tile;
