66a5ef464b2bdc1137cecb1a88b68d0f49fa61be
Mac Mittereder <macmitt2300@gmail.com>
2023-07-02 13:20:30 -0400

    release update (#35)
    * Added Lexicon and removed Blast Off Apps
    
    * Fixed image sizing and work place time worked
    
    * npm update
    
    * Fixed up some bugs

--- src/Components/Tile/Section.js
+++ src/Components/Tile/Section.js
@@ -3,25 +3,33 @@ const WorkSection = (props) => {
 
   return (
     <div>
-      <div className="flex items-center justify-center">
+      <div className="flex items-center justify-center w-auto h-64">
         {experience.logo ? (
           <img
             src={experience.logo}
             alt="Company Logo"
-            className="w-full lg:w-auto lg:h-64 text-center"
+            className={`${
+              experience.school ? "w-auto" : "w-full"
+            } h-auto text-center`}
           />
         ) : (
           ""
         )}
       </div>
-      <div className="flex flex-row">
+      <div className="flex flex-col md:flex-row">
         <p className="text-2xl font-bold text-left">
           {experience.company ?? experience.school}
         </p>
-        <p className="text-2xl">&nbsp;- {experience.location}</p>
+        <p className="text-2xl md:ml-2">
+          <span className="hidden md:inline">{" - "}</span>
+          {experience.location}
+        </p>
       </div>
-      <p className="text-2xl">{experience.position}</p>
-      <p className="text-lg font-bold">{experience.time}</p>
+      <p className="text-2xl underline">{experience.position}</p>
+      <div className="flex flex-col md:flex-row justify-start">
+        <p className="text-lg font-bold mr-1">{experience.time}</p>
+        <p className="text-lg font-bold">{experience.timeSummary ?? ""}</p>
+      </div>
       <ul className="ml-8">
         {experience.content.map((info, idx) => (
           <li className="list-disc text-md" key={idx}>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,31 +1,45 @@
 import Tile from "../Components/Tile/Tile";
 import Section from "../Components/Tile/Section";
+import LexiconLogo from "../images/lexicon-logo.png";
 import DollarBankLogo from "../images/dollar-bank-logo.png";
 import aSaLogo from "../images/aSa-logo.png";
-import ATSLogo from "../images/ats-logo.jpg";
-import BlastOffAppsLogo from "../images/blast-off-apps-logo.png";
+import ATSLogo from "../images/ats-logo.png";
+import SVCLogo from "../images/svc-logo.png";
 import moment from "moment";
 
 const Home = () => {
   const currentJobTime = () => {
-    var start = moment([2021, 8]);
+    var start = moment([2023, 3]);
     var end = moment([moment().year(), moment().month() + 2]);
 
     var months = moment.duration(end.diff(start)).months();
     var years = moment.duration(end.diff(start)).years();
 
-    return `(${years > 0 ? years + (years > 1 ? " yrs" : " yr") : ""} ${
+    return `(${years > 0 ? years + (years > 1 ? " yrs" : " yr") : ""}${
       months > 0 ? months + (months > 1 ? " mos" : " mo") : ""
     })`;
   };
 
   const workExperience = [
     {
+      company: "Lexicon",
+      logo: LexiconLogo,
+      location: "Remote",
+      position: "Full Stack Developer",
+      time: "August 2021 - Present ",
+      timeSummary: currentJobTime(),
+      content: [
+        "Developing Angular frontend and .NET backend SaaS for law firms.",
+        "Maintaining AngularJS legacy software for Cordell & Cordell law firms.",
+      ],
+    },
+    {
       company: "Dollar Bank",
       logo: DollarBankLogo,
       location: "Pittsburgh, PA",
       position: "Senior Application Developer",
-      time: "August 2021 - Present " + currentJobTime(),
+      time: "August 2021 - March 2023 ",
+      timeSummary: " (1 yr 8 mos)",
       content: [
         "Developed .NET applications from console applications to web services.",
         "Rewrote legacy ASP.NET web pages to .NET MVC.",
@@ -37,7 +51,8 @@ const Home = () => {
       logo: aSaLogo,
       location: "Murrysville, PA",
       position: "Software Developer II",
-      time: "May 2018 - August 2021 (3 yrs 4 mos)",
+      time: "May 2018 - August 2021 ",
+      timeSummary: " (3 yrs 4 mos)",
       content: [
         "Worked in two teams, Processing and Inventory Tracking, implementing new features using AngularJS and Entity Framework.",
         "Fixed customer issues all around the world including production database updates. ",
@@ -49,28 +64,18 @@ const Home = () => {
       logo: ATSLogo,
       location: "Greensburg, PA",
       position: "Software Engineer Intern",
-      time: "February 2017 - January 2018 (11 mos)",
+      time: "February 2017 - January 2018 ",
+      timeSummary: " (11 mos)",
       content: [
         "Developed disaster training software used by the Department of Defense using Pug, Node.js and Postgres.",
         "Assisted in performing quality assurance and software testing via various tests tools and performance metrics to ensure a responsive end-user experience.",
-      ],
-    },
-    {
-      company: "Blast Off Apps",
-      logo: BlastOffAppsLogo,
-      location: "Remote",
-      position: "Contractor",
-      time: "April 2015 - July 2016 (1 yr 4 mos)",
-      content: [
-        "Developed mobile apps for various clients for Android and iOS including FastMovie Photos, Words and Music, Vehicle List and Pinball Price.",
-        "Analyzed the market needs and designed the applications based on client's needs.",
-        "Investigated the applications performance using virtual Android and iOS Emulators.",
       ],
     },
   ];
 
   const education = {
     school: "Saint Vincent College",
+    logo: SVCLogo,
     location: "Latrobe, PA",
     position: "Computing and Information Science",
     time: "2014 - 2018",
@@ -95,7 +100,7 @@ const Home = () => {
       </Tile>
       <Tile title="Education" type="edu">
         <div>
-          <Section title={education.title} experience={education} />
+          <Section experience={education} />
         </div>
       </Tile>
     </div>
